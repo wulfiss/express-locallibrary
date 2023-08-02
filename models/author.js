097a4baf50dbec512b2ccce071e20cffcfeb78ca
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-08-02 19:28:03 -0300

    add view for authors list

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
     first_name: { type: String, required: true, maxLength: 100},
@@ -20,5 +21,16 @@ AuthorSchema.virtual('name').get(() => {
 AuthorSchema.virtual('url').get(() => {
     return `/catalog/author/${this._id}`;
 });
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+    if(DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) == 'Invalid DateTime'){
+        return ' ';
+    }
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+})
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+})
 
 module.exports = mongoose.model('Author', AuthorSchema);
