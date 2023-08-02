bc5744699811a845108ce3ffafb34672705d9d39
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-08-02 18:43:00 -0300

    add book list page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,34 +1,42 @@
-const Author = require('../models/author');
-const asyncHandler = require('express-async-handler');
+const Author = require("../models/author");
+const asyncHandler = require("express-async-handler");
 
-exports.author_list = asyncHandler( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author list');
+// Display list of all Authors.
+exports.author_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author list");
 });
 
-exports.author_detail = asyncHandler( async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
-})
+// Display detail page for a specific Author.
+exports.author_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+});
 
-exports.author_create_get = asyncHandler( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create GET');
-})
+// Display Author create form on GET.
+exports.author_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create GET");
+});
 
-exports.author_create_post = asyncHandler ( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create POST');
-})
+// Handle Author create on POST.
+exports.author_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author create POST");
+});
 
-exports.author_delete_get = asyncHandler( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author delete GET');
-})
+// Display Author delete form on GET.
+exports.author_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete GET");
+});
 
-exports.author_delete_post = asyncHandler( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author delete POST');
-})
+// Handle Author delete on POST.
+exports.author_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author delete POST");
+});
 
-exports.author_update_get = asyncHandler ( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author update GET');
-})
+// Display Author update form on GET.
+exports.author_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update GET");
+});
 
-exports.author_update_post =  asyncHandler ( async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author update POST');
-})
+// Handle Author update on POST.
+exports.author_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Author update POST");
+});
