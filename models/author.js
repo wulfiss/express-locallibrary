0cb9585e29bc6dc93745373dbbc5f8b1c28a3364
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-08-03 19:42:31 -0300

    Add books to genres

--- models/author.js
+++ models/author.js
@@ -9,7 +9,7 @@ const AuthorSchema = new Schema({
     date_of_death: { type: Date},
 });
 
-AuthorSchema.virtual('name').get(() => {
+AuthorSchema.virtual('name').get(function() {
     let fullname = '';
     if (this.first_name && this.family_name) {
         fullname = `${this.family_name}`, `${this.first_name}`;
@@ -18,7 +18,7 @@ AuthorSchema.virtual('name').get(() => {
     return fullname;
 });
 
-AuthorSchema.virtual('url').get(() => {
+AuthorSchema.virtual('url').get( function() {
     return `/catalog/author/${this._id}`;
 });
 
